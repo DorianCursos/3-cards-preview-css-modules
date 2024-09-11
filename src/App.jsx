20fe74901cdb943de113e21b45aa4910d17a9b9a
DorianCursos <doriancursos@gmail.com>
2024-09-11 14:05:50 +0200

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import Card from './components/card/Card';
 import Cards from './components/cards/Cards';
+import { CARDS_INFO } from './constants/cards-info';
 
 const App = () => {
 	return (
 		<div>
 			<h1>Curso de React - Práctica 1</h1>
 			<Cards>
-				<Card
-					icon='/assets/images/icon-sedans.svg'
-					title='SEDANS'
-					text='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
-					color='orange'
-					position='first'
-				/>
-				<Card
-					icon='/assets/images/icon-suvs.svg'
-					title='SUVS'
-					text='Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
-					color='blue'
-				/>
-				<Card
-					icon='/assets/images/icon-luxury.svg'
-					title='LUXURY'
-					text='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. '
-					color='green'
-					position='last'
-				/>
+				{CARDS_INFO.map(card => (
+					<Card key={card.id} {...card} />
+				))}
 			</Cards>
 		</div>
 	);
---
+++ src/constants/cards-info.js
@@ -0,0 +1,27 @@
+import { v4 } from 'uuid';
+
+export const CARDS_INFO = [
+	{
+		id: v4(),
+		icon: '/assets/images/icon-sedans.svg',
+		title: 'SEDANS',
+		text: 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+		color: 'orange',
+		position: 'first'
+	},
+	{
+		id: v4(),
+		icon: '/assets/images/icon-suvs.svg',
+		title: 'SUVS',
+		text: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
+		color: 'blue'
+	},
+	{
+		id: v4(),
+		icon: '/assets/images/icon-luxury.svg',
+		title: 'LUXURY',
+		text: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+		color: 'green',
+		position: 'last'
+	}
+];
